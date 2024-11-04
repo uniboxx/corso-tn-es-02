87bd154807c41c7b5ecc5973c09e79bfbde84a40
uniboxx <unibox@duck.com>
2024-11-04 20:01:24 +0100

    fine header

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// assegno l'elemento con id "search-btn" alla variabile "searchBtn"
 const searchBtn = document.getElementById('search-btn');
 
+// cliccando sull'elemento searchBtn (la lente) gli dico di prevenire l'azione di default del browser che tra le altre cose prevede un refresh della pagina
 searchBtn.addEventListener('click', (e) => {
   e.preventDefault();
 });
